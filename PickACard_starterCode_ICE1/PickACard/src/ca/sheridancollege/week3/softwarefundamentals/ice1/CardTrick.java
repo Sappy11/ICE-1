9919f3a5523e9438a3a3b3cb6145b33e01c79d87
Sappy11 <Singlsap@sheridancollege.ca>
2022-05-25 07:45:02 +0530

    PickACard updated

--- PickACard_starterCode_ICE1/PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ PickACard_starterCode_ICE1/PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,9 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.io.Console;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +23,23 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(ThreadLocalRandom.current().nextInt(1,14));
+            c.setSuit(Card.SUITS[ThreadLocalRandom.current().nextInt(0,4)]);
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        System.out.println("Enter suit (Hearts, Diamonds, Spades, CLubs) : ");
+        String suit = System.console().readLine();
+
+        System.out.println("Enter card number (1-13) : ");
+        int card = Integer.parseInt(System.console().readLine());
+
+        for(int i=0; i<magicHand.length; i++){
+            if(magicHand[i].getSuit().equals(suit) && magicHand[i].getValue() == card){
+                System.out.println("Card found in magic hand!!");
+                return;
+            }
+        }
+        System.out.println("Sorry card not in magic hand!!");
     }
     
 }
